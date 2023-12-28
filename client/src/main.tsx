c7615c57f990f8e33afae95578b8667980671d47
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2023-12-28 20:24:47 +0300

    Add serve package and Dashboard page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./styles/index.css";
+
 import { Theme } from "./components/Theme";
 import { AlertBox } from "./components/Alert";
 
 import { App } from "./pages/App";
-
 import { Login } from "./pages/Login";
 import { NotFound } from "./pages/NotFound";
+import { Dashboard } from "./pages/Dashboard";
 
 document.body.classList.add("bg-slate-200", "dark:bg-gray-900");
 
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+export const Dashboard = () => {
+  return (
+    <>
+      <main className="container mx-auto dark:text-white mt-5">x</main>
+    </>
+  );
+};
